ed16d768365613b887613ec37e4c7260496dbf03
Nomula, NareshReddy <NareshReddy.Nomula@cdk.com>
2018-11-06 18:10:38 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import { Container, Header, Content, Accordion } from "native-base";
+
+const dataArray = [
+    { title: "First Element", content: "Lorem ipsum dolor sit amet" },
+    { title: "Second Element", content: "Lorem ipsum dolor sit amet" },
+    { title: "Third Element", content: "Lorem ipsum dolor sit amet" }
+];
+
+export default class App extends Component {
+    render() {
+        return (
+            <Container>
+                <Header />
+                <Content padder>
+                    <Accordion dataArray={dataArray} icon="add" expandedIcon="remove" />
+                </Content>
+            </Container>
+        );
+    }
+}
